4415634be72b5a5631666ad2092d56e819e0071f
Andreas Eckner <andreas.eckner@extern.oth-regensburg.de>
2022-04-02 05:02:02 +0200

    Add exercise for 1.02 - Activities and Layouts - Intents

--- app/src/main/java/com/example/othregensburg/zapp/MainActivity.kt
+++ app/src/main/java/com/example/othregensburg/zapp/MainActivity.kt
@@ -12,6 +12,10 @@ class MainActivity : AppCompatActivity() {
         private val TAG: String? = MainActivity::class.simpleName
     }
 
+    // TODO (3) Create a new Activity "SecondActivity" in the same package, (Hint: File -> New -> Activity -> Empty Activity)
+    // TODO (4) Change the layout for the "SecondActivity" to show a TextView in the middle of the screen
+    // TODO (5) Add a new property for the EditText field and bind the view from the layout (Hint: use findViewById in the onCreate method)
+
     lateinit var helloWorldButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,4 +30,12 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+
+    // TODO (6) Add a method for the previously created button in the layout file
+    // TODO (7) Get the value from the EditText field
+    // TODO (8) Create an Intent to open the SecondActivity
+    // TODO (9) Put the String from the EditText to the Intent (Hint: Intent#putExtra(name, value))
+    // TODO (10) Start the SecondActivity
+    // TODO (11) In the SecondActivity read the data from the Intent and show the String in the TextView
+
 }
