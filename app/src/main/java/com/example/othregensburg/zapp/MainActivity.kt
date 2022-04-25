01cf4710e5bd4fe5cf100d3ae7bcf0c9313f76c8
HuberSimon <simon.huber@st.oth-regensburg.de>
2022-04-25 09:19:42 +0200

    first commit feature 1

--- app/src/main/java/com/example/othregensburg/zapp/MainActivity.kt
+++ app/src/main/java/com/example/othregensburg/zapp/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.othregensburg.zapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+
+
+
+//feature-1 Simon
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
